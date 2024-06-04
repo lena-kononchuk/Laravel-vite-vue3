32693042c99c232f7f3606fc0b33c4bb7dcbf8da
lenakononchuk <lena@rockingtech.co.uk>
2024-06-04 22:34:52 +0300

    Base of project

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Avenir:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Laravel with Vite and Vue 3</title>
     @vite(['resources/less/app.less', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div>
+    @yield('content')
+
+    @vite(['resources/js/app.js'])
 </body>
 </html>
